c2dd0d8e118f53e5f7dd3fc459f8ab78208941ee
marci papineau <marcidenmark@hotmail.com>
2018-01-27 11:15:29 +0100

    cities creating a pending promise but not errors

--- src/scripts-2.js
+++ src/scripts-2.js
@@ -6,27 +6,31 @@ document.addEventListener("DOMContentLoaded", () => {
 const endpoint = 'https://gist.githubusercontent.com/marcidenmark/b7d5160b19cf97b6d08c64dd68c5cc4c/raw/204fb9fd3f8f03688f9a6313ca53b7ce9956663a/flights.json';
 const departure = document.getElementById("departureCity");
 
-
+cities =
 	fetch(endpoint)
 		.then(rawdata => rawdata.json())//fetch can't handle json.
 		.then((data) => {
 			departureCity.innerHTML = "";
  			console.log(data);
+ 				console.log(departureCity);
+
   	});
-	console.log(departure);
+	console.log(cities);
+	// console.log(departure);
 
-const button = document.getElementByClassName("mdc-button");
-console.log(button);
-
-	button.addEventListener("click", (event) => {
-		console.log(event);
-		console.log(event.target);
-	});
 
 	departureCity.addEventListener("click", (event) => {
 	console.log(event);
 	console.log(event.target);
   });
+
+// const button = document.getElementByClassName("mdc-button");
+// console.log(button);
+
+// 	button.addEventListener("click", (event) => {
+// 		console.log(event);
+// 		console.log(event.target);
+// 	});
 
 
 
