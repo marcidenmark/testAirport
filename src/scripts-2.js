e81b43e0e69f4c8830f1d5088991693f6a0bf256
marci papineau <marcidenmark@hotmail.com>
2018-01-28 13:57:43 +0100

    tried refactoring code to access data. didn't work

--- src/scripts-2.js
+++ src/scripts-2.js
@@ -8,19 +8,18 @@
 
 const destination = document.getElementById("departureCity");
 
-const callJson = (destination, callback) => {
 const url = 'https://gist.githubusercontent.com/marcidenmark/b7d5160b19cf97b6d08c64dd68c5cc4c/raw/204fb9fd3f8f03688f9a6313ca53b7ce9956663a/flights.json';
+console.log(url);
+const callJson = (destination, flights) => {
 
 	fetch(url)
-		.then(rawdata => rawdata.json())//fetch can't handle json.
+		.then(rawdata => rawdata.json())
 		.then((data) => {
-			callback(data);
-			// data.results((destination) => {
-			// 	const item = `<input> ${flights.destination}</input>`;
-			// 	departureCity.insertValue(destination.value);
+			flights(data);
 			});
 };
 console.log(callJson);
+console.log(destination);
 
 // <input type="search" id="address-input" placeholder="Where are we going?" />
 	departureCity.addEventListener("click", (event) => {
