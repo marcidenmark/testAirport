b378d895095e2274c80b3ed954377927f1683d16
marci papineau <marcidenmark@hotmail.com>
2018-01-28 14:07:24 +0100

    starting again from here

--- src/scripts-2.js
+++ src/scripts-2.js
@@ -2,7 +2,7 @@
 //   console.log("DOM fully loaded and parsed");
 //   // Add here your addEventListener code
 // });
-
+//back to commit 7afb76f6b2713e0ff33a0f67d9fa6421ce65d197
 const endpoint = 'https://gist.githubusercontent.com/marcidenmark/b7d5160b19cf97b6d08c64dd68c5cc4c/raw/204fb9fd3f8f03688f9a6313ca53b7ce9956663a/flights.json';
 // const departure = document.getElementById("departureCity");
 // citties = this creates a promise, but it is "pending" when seen in the console"
