41d883f41c9d244d0abc0f20a5329994cdeb1d61
marci papineau <marcidenmark@hotmail.com>
2018-03-20 11:43:25 +0100

    tried something new

--- src/scripts-6.js
+++ src/scripts-6.js
@@ -1,24 +1,45 @@
 
+  // function createNode(element) {
+  //     return document.createElement(element);
+  // }
 
+  // function append(parent, el) {
+  //   return parent.appendChild(el);
+  // }
+
+  // const ul = document.getElementById('departureCity');
+  // const url = 'https://gist.githubusercontent.com/marcidenmark/b7d5160b19cf97b6d08c64dd68c5cc4c/raw/204fb9fd3f8f03688f9a6313ca53b7ce9956663a/flights.json';
+  // fetch(url)
+  // .then((resp) => resp.json())
+  // .then(function(data) {
+  //   let departureCity = data.results;
+  //   return departureCity.map(function(Destination) {
+  //     let li = createNode('li'),
+  //         span = createNode('span');
+  //     span.innerHTML = `${Destination.name.first} ${Destination.name.last}`;
+  //     append(li, span);
+  //     append(ul, li);
+  //   })
+  // })
+  // .catch(function(error) {
+  //   console.log(JSON.stringify(error));
+  // });
+
+// _________________________________________
+const cities = document.getElementById('cities');
 const endpoint = 'https://gist.githubusercontent.com/marcidenmark/b7d5160b19cf97b6d08c64dd68c5cc4c/raw/204fb9fd3f8f03688f9a6313ca53b7ce9956663a/flights.json';
 // const departure = document.getElementById("departureCity");
 // citties = this creates a promise, but it is "pending" when seen in the console"
-const cities =
     fetch(endpoint)
     .then(rawdata => rawdata.json()) //fetch can't handle json.
     .then((data) => {
-        console.log(data);
-        // console.log(departureCity); //giving in input id html
-
+    	let cities = data.results;
+        // console.log(data);
+        console.log(cities); //giving in input id html
     });
-// console.log(cities);
+ //console.log(cities);
 // console.log(departure);
-departureCity.addEventListener("click", (event) => {
-    // console.log(event);
+cities.addEventListener("click", (event) => {
+    console.log(event);
     console.log(event.target);
 });
-
-let placesAutocomplete = places({
-    container: document.querySelector('#address-input')
-});
-// console.log(placesAutocomplete);
